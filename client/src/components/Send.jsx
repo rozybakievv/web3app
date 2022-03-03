4f469ed0bb4f84be57472d6af0caec659a1357ff
Guyass Rozybakiev <rozzybakiev@gmail.com>
2022-03-02 22:02:10 -0500

    update send component

--- client/src/components/Send.jsx
+++ client/src/components/Send.jsx
@@ -4,7 +4,6 @@ import { TransactionContext } from "../App";
 const Send = () => {
     const { formAddressContext, formAmountContext, sendTransaction, successContext } = useContext(TransactionContext);
     
-
     const sendT = async () => {
         const address = document.getElementById("address");
         const amount = document.getElementById("amountEth");
@@ -24,20 +23,14 @@ const Send = () => {
                 </div>
             </div>
 
-            <form autoComplete="off">
+            <form autoComplete="off" className="w-96">
                 <div className="mb-6">
                     <label htmlFor="address" className="block mb-2 text-md text-neutral-700">Address</label>
                     <input type="text" id="address" placeholder="0x" required className="block w-full p-2 border-2 outline-none graycolor rounded-xl border-neutral-700" />
                 </div>
-                <div className="grid grid-cols-2 gap-4 mb-6">
-                    <div>
-                        <label htmlFor="address" className="block mb-2 text-md text-neutral-700">Amount (ETH)</label>
-                        <input type="number" id="amountEth" placeholder="eth" required className="block w-full p-2 border-2 outline-none graycolor rounded-xl border-neutral-700" />
-                    </div>
-                    <div>
-                    <label htmlFor="amountUsd" className="block mb-2 text-md text-neutral-700">Amount (USD)</label>
-                        <input type="number" id="amountUsd" placeholder="$" required className="block w-full p-2 border-2 outline-none graycolor rounded-xl border-neutral-700" />
-                    </div>
+                <div className="mb-6">
+                    <label htmlFor="address" className="block mb-2 text-md text-neutral-700">Amount (ETH)</label>
+                    <input type="number" id="amountEth" placeholder="eth" required className="block w-full p-2 border-2 outline-none graycolor rounded-xl border-neutral-700" />
                 </div>
                 <button onClick={sendT} className="inline-block w-full h-10 px-4 py-1 text-sm text-white transition duration-200 ease-in-out border-b-4 bg-neutral-700 shadow-effect rounded-xl border-neutral-500 hover:translate-y-1">Send</button>
             </form>
--- client/src/components/Transfers.jsx
+++ client/src/components/Transfers.jsx
@@ -8,6 +8,7 @@ const Receive = () => {
     useEffect(() => {
         checkTransactions();
         getAllTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     
     return (
